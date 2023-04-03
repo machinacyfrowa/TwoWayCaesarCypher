01b7ec7cab691314b8799f62f9bba7c2f2a2d8fd
pzawadzki <admin@machinacyfrowa.pl>
2023-04-03 13:09:06 +0200

    table encryption

--- Cezar/Program.cs
+++ Cezar/Program.cs
@@ -46,9 +46,35 @@ string FromAsciiCaesar(string encryptedText, int key)
     }
     return clearText;
 }
+
+string ToArrayCaesar(string clearText, int key)
+{
+	string chars = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUVWXYZŹŻ";
+	char[] charArray = chars.ToCharArray();
+	clearText = clearText.ToUpper();
+	string encryptedText = "";
+	foreach (char c in clearText) 
+	{
+		//znajdz w tablicy charArray literkę do zakodowania i zapisz jej indeks do charIndex
+		int charIndex = Array.IndexOf(charArray, c);
+		//dodaj wartość klucza
+		charIndex += key;
+		//jeżeli poza tablicą to zawróć na początek
+		if(charIndex > charArray.Length) 
+		{ 
+			charIndex -= charArray.Length; 
+		}
+		//zapisz zaszyfrowany znak do docelowego tekstu
+		encryptedText += charArray[charIndex];
+	}
+	return encryptedText;
+}
+
 string text = "TEKST DO ZASZYFROWANIA";
 string cypher = ToAsciiCaesar(text, 3);
-Console.WriteLine("Zaszyfrowany tekst: " + cypher);
+Console.WriteLine("Zaszyfrowany tekst (ascii): " + cypher);
 
 text = FromAsciiCaesar(cypher, 3);
-Console.WriteLine("Odszyfrowany tekst: " + text);
+Console.WriteLine("Odszyfrowany tekst (ascii): " + text);
+
+Console.WriteLine("Zaszyfrowany tekst (table): " + ToArrayCaesar("Zażółć gęślą jaźń", 3));
